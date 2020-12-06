080d35925a60dad5b263668ac01903e25cadbac0
Your Name <kisandy@yandex.ru>
2020-12-06 20:43:34 +0300

    complete hw3

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -16,10 +16,48 @@ from _camtrack import *
 
 import sortednp as snp
 import cv2
+import pims
 
 
-# podgone zone
-triang_params = TriangulationParameters(1.2, 1.0, 0.1)
+proj_err = 3.0
+triang_params = TriangulationParameters(proj_err, 5.0, 0.5)
+
+
+def init_tracking(
+        intrinsic_mat,
+        corner_storage,
+        rgb_sequence: pims.FramesSequence
+):
+    frame_1 = 0
+    view_mat_1 = eye3x4()
+    min_cos = 1
+    max_len_ps = 0
+    for i in range(1, len(rgb_sequence)):
+        corr = build_correspondences(corner_storage[frame_1], corner_storage[i])
+        if len(corr.ids) < 10:
+            continue
+        E, _ = cv2.findEssentialMat(
+            corr.points_1,
+            corr.points_2,
+            intrinsic_mat,
+            threshold=proj_err
+        )
+        _, R, t, _ = cv2.recoverPose(E, corr.points_1, corr.points_2)
+        view_mat_2 = np.hstack((R, -t))
+        points3d, ids, median_cos = triangulate_correspondences(
+            corr,
+            view_mat_1,
+            view_mat_2,
+            intrinsic_mat,
+            triang_params
+        )
+        min_cos = min(min_cos, median_cos)
+        max_len_ps = max(max_len_ps, len(points3d))
+
+        if len(points3d) > 10:
+            return (frame_1, view_mat3x4_to_pose(view_mat_1)), (i, view_mat3x4_to_pose(view_mat_2))
+
+    return None, None
 
 
 def print_proc_info(cur_frame, frame_cnt, inliers_cnt):
@@ -32,15 +70,15 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None) \
         -> Tuple[List[Pose], PointCloud]:
-    if known_view_1 is None or known_view_2 is None:
-        raise NotImplementedError()
-
     rgb_sequence = frameseq.read_rgb_f32(frame_sequence_path)
     intrinsic_mat = to_opencv_camera_mat3x3(
         camera_parameters,
         rgb_sequence[0].shape[0]
     )
 
+    if known_view_1 is None or known_view_2 is None:
+        known_view_1, known_view_2 = init_tracking(intrinsic_mat, corner_storage, rgb_sequence)
+        print('Initial frames:', known_view_1[0], known_view_2[0])
 
     corrs = build_correspondences(corner_storage[known_view_1[0]], corner_storage[known_view_2[0]])
 
@@ -80,9 +118,10 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     objectPoints=known_3d,
                     imagePoints=known_2d,
                     cameraMatrix=intrinsic_mat,
-                    distCoeffs=None
+                    distCoeffs=None,
+                    reprojectionError=proj_err
                 )
-                inliers_np = np.array(inliers, dtype=int)
+
                 inliers_count = len(inliers)
                 if inliers_count > 0:
                     view_mats[i] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
@@ -92,12 +131,11 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                 print_proc_info(i, frame_count, 0)
             if view_mats[i] is None:
                 continue
-            cur_corner = filter_frame_corners(corner_storage[i], inliers_np.flatten())
 
             for j in range(frame_count):
                 if view_mats[j] is None:
                     continue
-                corrs = build_correspondences(corner_storage[j], cur_corner)
+                corrs = build_correspondences(corner_storage[j], corner_storage[i])
                 if len(corrs.ids) == 0:
                     continue
                 points3d, ids, _ = triangulate_correspondences(
